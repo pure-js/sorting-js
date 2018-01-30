6bd6a672ebf99359bd7d92069d342b3c0b809f78
pure-js <pure-js@users.noreply.github.com>
2018-01-30 16:17:01 +0300

    fix syntax

--- generate-arrays.js
+++ generate-arrays.js
@@ -3,10 +3,12 @@ function getRandom(min, max) {
 }
 
 function generateArray(min = 12, max = 20) {
-  let arr = [];
-  let length = getRandom(min, max);
+  const arr = [];
+  const length = getRandom(min, max);
   for (let i = 0; i < length; i++) {
     arr.push(getRandom(0, 1000));
   }
   return arr;
 }
+
+export default generateArray;
