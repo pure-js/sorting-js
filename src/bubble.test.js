40cadb6d8239cc5d0a8c8ac3b4c8f3cbb33588b7
pure-js <pure-js@users.noreply.github.com>
2018-01-30 14:48:06 +0300

    fix test for bubble sorting

--- src/bubble.test.js
+++ src/bubble.test.js
@@ -2,6 +2,6 @@ import bubble from './bubble';
 
 test('contains spec with an expectation', () => {
   const arr = [7, 9, 2, 120, 0, 67, 4, 560, 76, 23, 14, 5, 123, 78];
-  const sortedArr = [0, 2, 4, 5, 7, 9, 14, 23, 67, 120, 560, 76, 123, 78];
+  const sortedArr = [0, 2, 4, 5, 7, 9, 14, 23, 67, 76, 78, 120, 123, 560];
   expect(bubble(arr)).toEqual(sortedArr);
 });
