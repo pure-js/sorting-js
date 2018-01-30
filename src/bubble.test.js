63ac4e3702ae7a8c83a4b69dae957e8328521110
pure-js <pure-js@users.noreply.github.com>
2018-01-30 18:13:17 +0300

    improve syntax

--- src/bubble.test.js
+++ src/bubble.test.js
@@ -1,7 +1,7 @@
-import bubble from './bubble';
+import bubbleSort from './bubble';
 
-test('contains spec with an expectation', () => {
+test('sort correctly', () => {
   const arr = [7, 9, 2, 120, 0, 67, 4, 560, 76, 23, 14, 5, 123, 78];
   const sortedArr = [0, 2, 4, 5, 7, 9, 14, 23, 67, 76, 78, 120, 123, 560];
-  expect(bubble(arr)).toEqual(sortedArr);
+  expect(bubbleSort(arr)).toEqual(sortedArr);
 });
